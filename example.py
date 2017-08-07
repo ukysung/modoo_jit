fa1a231ac8ecc67c9d0254a632367ac05d49ec40
ukysung <ukysung@gmail.com>
2017-08-07 18:31:06 +0900

    성능 평가 추가

--- example.py
+++ example.py
@@ -1,11 +1,30 @@
 import modoo_jit
+import time
 
+def none_jit_func(a):
+    b = a ** 2
+    return b
 
 @modoo_jit.jit
-def f(a):
+def jit_func(a):
     b = a ** 2
-    print('b : {}'.format(b))
     return b
 
 if __name__ == '__main__':
-    f(123)
+
+    start = time.time()
+    for i in range(1000):
+        none_jit_func(i)
+    end = time.time()
+    elapsed = end - start
+    print('none_jit_func elapsed time : {}'.format(elapsed))
+
+    # warming up => prepare binary
+    jit_func(1)
+
+    start = time.time()
+    for i in range(1000):
+        jit_func(i)
+    end = time.time()
+    elapsed = end - start
+    print('jit_func elapsed time : {}'.format(elapsed))
